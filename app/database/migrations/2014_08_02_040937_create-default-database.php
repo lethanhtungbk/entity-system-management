fb461793d581aa5af0edaa76cb054438c0c7a9dc
Le Thanh Tung <lethanhtungbk@gmail.com>
2014-08-02 12:59:10 +0700

    Prototype Developing [1]

--- app/database/migrations/2014_08_02_040937_create-default-database.php
+++ app/database/migrations/2014_08_02_040937_create-default-database.php
@@ -5,24 +5,58 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateDefaultDatabase extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		//
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('field_types', function($table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('display');
+            $table->integer('value_type')->default(1);
+        });
+        
+        
+        Schema::create('fields', function($table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->integer('field_type_id');
+            $table->string('object_dependences');
+            $table->string('field_dependences');
+            $table->string('value_in_object');
+            $table->string('value_in_field');
+            
+        });    
+        
+        
+        
+        Schema::create('field_values', function($table) {
+            $table->increments('id');
+            $table->integer('field_id');
+            $table->string('value');
+            
+        });
+        
+        
+        Schema::create('groups', function($table) {
+            $table->increments('id');
+            $table->integer('name');
+            $table->string('fields');            
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		//
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('field_types');
+        Schema::dropIfExists('fields');
+        Schema::dropIfExists('field_values');
+        Schema::dropIfExists('groups');
+    }
 
 }
---
+++ app/database/seeds/EMS.php
@@ -0,0 +1,61 @@
+<?php
+
+class EMS extends Seeder {
+
+    public function run() {
+        //drop all recordsets
+        DB::table('field_types')->delete();
+        DB::table('fields')->delete();
+        DB::table('field_values')->delete();
+
+        //field_types
+        $id = 1;
+        DB::table('field_types')->insert(
+                array(
+                    //single value
+                    array('id' => 1, 'name' => 'Text Field', 'display' => 'textfield','value_type' => 1),
+                    array('id' => 2,'name' => 'Text Area', 'display' => 'textfield','value_type' => 1),
+                    array('id' => 3,'name' => 'Image', 'display' => 'textfield','value_type' => 1),
+                    array('id' => 4,'name' => 'Audio', 'display' => 'textfield','value_type' => 1),
+                    array('id' => 5,'name' => 'Video', 'display' => 'textfield','value_type' => 1),
+                    array('id' => 6,'name' => 'Attachment', 'display' => 'textfield','value_type' => 1),
+                    array('id' => 7,'name' => 'RichText Area', 'display' => 'textfield','value_type' => 1),
+                    array('id' => 8,'name' => 'RichText Area', 'display' => 'textfield','value_type' => 1),
+                    //multi value - single selected
+                    array('id' => 9,'name' => 'Dropdown', 'display' => 'dropdown','value_type' => 2),
+                    array('id' => 10,'name' => 'RadioBox', 'display' => 'radio','value_type' => 2),
+                    //multi value - single selected
+                    array('id' => 11,'name' => 'ListBox', 'display' => 'list','value_type' => 3),
+                    array('id' => 12,'name' => 'CheckBox', 'display' => 'checkbox','value_type' => 3),
+                )
+        );
+        
+        //fields
+        DB::table('fields')->insert(
+                array(
+                    //student
+                    array('id' => 1, 'name' => 'Student Name', 'field_type_id' => '1','object_dependences' => '','field_dependences' => '','value_in_object' => '','value_in_field' => ''),
+                    array('id' => 2, 'name' => 'City', 'field_type_id' => '9','object_dependences' => '','field_dependences' => '','value_in_object' => '','value_in_field' => ''),
+                    array('id' => 3, 'name' => 'District', 'field_type_id' => '9','object_dependences' => '','field_dependences' => '2','value_in_object' => '','value_in_field' => ''),
+                    array('id' => 4, 'name' => 'Class', 'field_type_id' => '9','object_dependences' => '2','field_dependences' => '','value_in_object' => '','value_in_field' => ''),
+                    
+                    //class
+                    array('id' => 5, 'name' => 'Class Name', 'field_type_id' => '1','object_dependences' => '','field_dependences' => '','value_in_object' => '','value_in_field' => ''),
+                    array('id' => 6, 'name' => 'Head student', 'field_type_id' => '9','object_dependences' => '','field_dependences' => '','value_in_object' => '1','value_in_field' => ''),
+                    
+                    //subject
+                    array('id' => 7, 'name' => 'Subject Name', 'field_type_id' => '1','object_dependences' => '','field_dependences' => '','value_in_object' => '','value_in_field' => ''),
+                    
+                    //mark
+                    array('id' => 8, 'name' => 'Mark Name', 'field_type_id' => '1','object_dependences' => '','field_dependences' => '','value_in_object' => '','value_in_field' => ''),
+                    array('id' => 9, 'name' => 'Student Name', 'field_type_id' => '9','object_dependences' => '','field_dependences' => '','value_in_object' => '1','value_in_field' => ''),
+                    array('id' => 10, 'name' => 'Value', 'field_type_id' => '10','object_dependences' => '','field_dependences' => '','value_in_object' => '','value_in_field' => ''),
+                )
+        );
+        
+        
+        
+
+    }
+
+}
